f6f4ad08c681e68b72e4348294cf431acfd51e16
= <=marieshix98@gmail.com>
2021-10-21 15:23:43 +0300

    remove movies from the playlist

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import styled from "styled-components"
 
  const Search = ({searchMovies, handleChange}) => {
     return (
-        <div>
+        <SearchWrapper>
             <input type="text" placeholder="Search Movies" value={searchMovies} onChange={handleChange}/>
-        </div>
+        </SearchWrapper>
     )
 }
 
 export default Search;
+export const SearchWrapper= styled.div`
+input{
+    max-width: 400px;
+    width: 100%;
+    padding: 10px 20px;
+    border: 2px solid #000;
+    background-color: #fff;
+    border-radius: 5px;
+    font-size: 24px;
+    font-weight: 500;
+}
+
+`
--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,33 +1,30 @@
-import React, {useState} from 'react'
-import {ThemeProvider} from 'styled-components';
-import {DarkTheme, LightTheme, GlobalStyles} from "./Theme"
-import { FaBeer,FcHeadset } from 'react-icons/fa';
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { DarkTheme, LightTheme, GlobalStyles } from "./Theme";
+import { BsBrightnessHighFill, BsFillMoonFill } from "react-icons/bs";
+
 import { IconContext } from "react-icons";
 
+function Toggle({ children }) {
+  const [lightTheme, setTheme] = useState(!true);
 
-function Toggle({children}) {
-    const [lightTheme, setTheme] = useState(true)
+  const theme = lightTheme ? LightTheme : DarkTheme;
 
-const theme = lightTheme ? LightTheme : DarkTheme
-
-    const handleToggle = () =>{
-        setTheme(prevMode => !prevMode)
-    }
-    return (
-       <ThemeProvider theme ={theme}>
-           <GlobalStyles/>
-           {children}
-           <IconContext.Provider value={{fontSize:"90px"}}>
-               <div >
-               <FaBeer onClick={handleToggle}/>
-               </div>
-        
-           </IconContext.Provider>
-           
-       </ThemeProvider>
-           
-    
-    )
+  const handleToggle = () => {
+    setTheme((prevMode) => !prevMode);
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {children}
+      <IconContext.Provider value={{ fontSize: "90px" }}>
+        <IconWrapper onClick={handleToggle}>
+          {lightTheme ? <BsFillMoonFill/> : <BsBrightnessHighFill />}
+        </IconWrapper>
+      </IconContext.Provider>
+    </ThemeProvider>
+  );
 }
 
-export default Toggle
+export default Toggle;
+export const IconWrapper = styled.div``;
